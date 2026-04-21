8b44cd36221bf08a6bdce2b73d67bfb32f6cbc57
tiyegordon <tiye.gordon@gmail.com>
2026-04-21 12:09:10 -0400

    readme

---
+++ holbertonschool-web_back_end/python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function add that takes a float a and a float b"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a + b"""
+    return a + b
---
+++ holbertonschool-web_front_end/html_advanced/README.md
@@ -0,0 +1,1 @@
+This module is for HTML Advanced
