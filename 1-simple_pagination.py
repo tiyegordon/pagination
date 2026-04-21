4987def3402720413dc2637896aaaf59e963e79c
tiyegordon <tiye.gordon@gmail.com>
2026-04-21 12:57:17 -0400

    add simple pagination

---
+++ 1-simple_pagination.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Simple pagination using offset strategy.
+"""
+import csv
+import math
+from typing import List
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return (start_index, end_index) for given page and page_size. """
+    start = (page -1) * page_size
+    end = start + page_size
+    return (start, end)
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+    
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset [1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int =10) -> List[List]:
+        """Return the correct page of data from the dataset."""
+        assert isinstance(page, int) and isinstance(page_size, int), \
+                "page and page_size must be integers"
+        assert page > 0 and page_size >0, \
+                "page and page_size must be greater than 0"
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
+
+
+
