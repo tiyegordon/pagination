d34d4ecf0baa4bed9349cd7129afe899de02834b
tiyegordon <tiye.gordon@gmail.com>
2026-05-24 19:26:32 -0400

    Add isDirector and executeWork functions

--- holbertonschool-web_react/TypeScript/task_2/js/main.ts
+++ holbertonschool-web_react/TypeScript/task_2/js/main.ts
@@ -44,3 +44,17 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
